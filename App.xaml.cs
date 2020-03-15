45e797c8b9a87ebdb085bc46581d5cbbe7049476
Rick Agustin <rick.agustin@hotmail.com>
2020-03-15 16:54:23 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfDI {
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application {
+    protected override void OnStartup (StartupEventArgs e)
+    {
+      base.OnStartup (e);
+
+      var collection = new ServiceCollection ();
+      collection.AddTransient <MainWindow> ();
+      collection.AddScoped<TestClass>();
+
+      var provider = collection.BuildServiceProvider ();
+      var mainWindow = provider.GetRequiredService <MainWindow> ();
+
+      mainWindow.Show ();
+    }
+  }
+}
---
+++ TestClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfDI {
+  public class TestClass {
+    public string Message => "Hello!";
+  }
+}
